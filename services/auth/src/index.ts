507d69c47a02b60aed8f8aa8e806ca1b82e5e22b
kanhaiya3152 <kanhaiya3152@gmail.com>
2026-03-17 11:38:37 +0530

    feat: Add authentication service with login, role management, and middleware for JWT verification

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoute from "./routes/auth.js"
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.use(cors());
 
