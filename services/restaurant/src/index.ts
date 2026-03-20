0388507be73e82ccef479e0b273a26ff294abeb1
kanhaiya3152 <kanhaiya3152@gmail.com>
2026-03-20 21:56:09 +0530

    feat(routes): add menu items route to the restaurant service

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import cors from "cors";
 import restaurantRoute from "./routes/restuarant.js"
+import itemRoute from "./routes/menuitems.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/item", itemRoute);
 
 const PORT = process.env.PORT || 5001;
 
