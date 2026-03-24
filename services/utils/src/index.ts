828588e7e3fd74e673d2d0883d6e935c0c951851
kanhaiya3152 <kanhaiya3152@gmail.com>
2026-03-24 09:45:24 +0530

    implement rabbitmq connection

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 import cloudinary from "cloudinary";
 import cors from "cors";
 import uploadRoutes from "./routes/cloudinary.js";
-// import paymentRoutes from "./routes/payment.js";
-// import { connectRabbitMQ } from "./config/rabbitmq.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+import paymentRoutes from "./routes/payment.js";
 
 dotenv.config();
 
-// connectRabbitMQ();
+connectRabbitMQ();
 
 const app = express();
 
@@ -30,7 +30,7 @@ cloudinary.v2.config({
 });
 
 app.use("/api", uploadRoutes);
-// app.use("/api/payment", paymentRoutes);
+app.use("/api/payment", paymentRoutes);
 
 const PORT = process.env.PORT || 5002;
 
