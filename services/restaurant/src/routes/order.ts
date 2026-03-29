2e8e1dc37f5b436c921b580e2ce40658e52a2e8d
kanhaiya3152 <kanhaiya3152@gmail.com>
2026-03-29 11:37:36 +0530

    add the new route methods

--- services/restaurant/src/routes/order.ts
+++ services/restaurant/src/routes/order.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuth, isSeller } from "../middlewares/isAuth.js";
-import { createOrder, fetchOrderForPayment, fetchRestaurantOrders, fetchSingleOrder, getMyOrders, updateOrderStatus } from "../controllers/order.js";
+import { assignRiderToOrder, createOrder, fetchOrderForPayment, fetchRestaurantOrders, fetchSingleOrder, getCurrentOrderForRider, getMyOrders, updateOrderStatus, updateOrderStatusRider } from "../controllers/order.js";
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.get("/payment/:id", fetchOrderForPayment);
 router.get("/:restaurantId",isAuth, isSeller, fetchRestaurantOrders);
 router.get("/restaurant/:restaurantId", isAuth, isSeller, fetchRestaurantOrders);
 router.put("/:orderId",isAuth, isSeller, updateOrderStatus);
-
+router.put("/assign/rider", assignRiderToOrder);
+router.get("/current/rider", getCurrentOrderForRider);
+router.put("/update/status/rider", updateOrderStatusRider);
 export default router;
---
+++ services/rider/src/routes/rider.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { isAuth } from "../middlewares/isAuth.js";
+import { acceptOrder, addRiderProfile, fetchMyCurrentOrder, fetchMyProfile, toggleRiderAvailablity, updateOrderStatus } from "../controllers/rider.js";
+import uploadFile from "../middlewares/multer.js";
+
+const router  = express.Router();
+
+router.get("/myprofile", isAuth, fetchMyProfile);
+router.patch("/toggle", isAuth, toggleRiderAvailablity);
+router.post("/new", isAuth, uploadFile, addRiderProfile);
+router.post("/accept/:orderId", isAuth, acceptOrder);
+router.get("/order/current", isAuth, fetchMyCurrentOrder);
+router.put("/order/update/:orderId", isAuth, updateOrderStatus);
+
+export default router;
